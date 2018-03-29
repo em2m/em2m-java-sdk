77d7869243f2cb6a80538115656eed97b7225f73
Christopher Lakey <christopherlakey@gmail.com>
2018-03-29 18:49:09 -0400

    Simplex optimizations

--- simplex/src/main/java/io/em2m/simplex/model/expr.kt
+++ simplex/src/main/java/io/em2m/simplex/model/expr.kt
@@ -10,7 +10,25 @@ interface Part {
     fun call(context: ExprContext): Any?
 }
 
-data class PipePart(val key: Key, val handler: KeyHandler, val transforms: List<PipeTransform> = emptyList()) : Part {
+data class KeyOnlyPipePart(val key: Key, val handler: KeyHandler) : Part {
+    override fun call(context: ExprContext): Any? {
+        val contextKeys: KeyResolver? = context["keys"] as? KeyResolver
+        val handler = contextKeys?.find(key) ?: handler
+        return handler.call(key, context)
+    }
+}
+
+data class SingleTransformPipePart(val key: Key, val handler: KeyHandler, val transform: PipeTransform) : Part {
+
+    override fun call(context: ExprContext): Any? {
+        val contextKeys: KeyResolver? = context["keys"] as? KeyResolver
+        val handler = contextKeys?.find(key) ?: handler
+        val initial = handler.call(key, context)
+        return transform.transform(initial, context)
+    }
+}
+
+data class MultiTransformPipePart(val key: Key, val handler: KeyHandler, val transforms: List<PipeTransform> = emptyList()) : Part {
 
     override fun call(context: ExprContext): Any? {
         val contextKeys: KeyResolver? = context["keys"] as? KeyResolver
@@ -20,6 +38,7 @@ data class PipePart(val key: Key, val handler: KeyHandler, val transforms: List<
     }
 }
 
+
 data class ConstPart(val value: String) : Part {
 
     override fun call(context: ExprContext): Any? {
@@ -27,21 +46,61 @@ data class ConstPart(val value: String) : Part {
     }
 }
 
-data class Expr(val parts: List<Part>) {
+interface Expr {
+    fun call(context: ExprContext): Any?
+}
 
-    fun call(context: ExprContext): Any? {
-        return if (parts.size == 1) {
-            parts[0].call(context)
-        } else {
-            val builder = StringBuilder()
-            parts.forEach { part ->
-                val value = part.call(context)
-                if (value != null) {
-                    builder.append(value)
-                }
-            }
-            builder.toString()
+data class SinglePartExpr(val part: Part) : Expr {
+    override fun call(context: ExprContext): Any? {
+        return part.call(context)
+    }
+}
+
+data class TwoPartExpr(val first: Part, val second: Part) : Expr {
+    override fun call(context: ExprContext): Any? {
+        val builder = StringBuilder()
+        val v1 = first.call(context)
+        val v2 = second.call(context)
+        if (v1 != null) {
+            builder.append(v1)
         }
+        if (v2 != null) {
+            builder.append(v2)
+        }
+        return builder.toString()
+    }
+}
+
+data class ThreePartExpr(val first: Part, val second: Part, val third: Part) : Expr {
+    override fun call(context: ExprContext): Any? {
+        val builder = StringBuilder()
+        val v1 = first.call(context)
+        val v2 = second.call(context)
+        val v3 = third.call(context)
+        if (v1 != null) {
+            builder.append(v1)
+        }
+        if (v2 != null) {
+            builder.append(v2)
+        }
+        if (v3 != null) {
+            builder.append(v3)
+        }
+        return builder.toString()
+    }
+}
+
+data class MultiPartExpr(val parts: List<Part>) : Expr {
+
+    override fun call(context: ExprContext): Any? {
+        val builder = StringBuilder()
+        parts.forEach { part ->
+            val value = part.call(context)
+            if (value != null) {
+                builder.append(value)
+            }
+        }
+        return builder.toString()
     }
 }
 
