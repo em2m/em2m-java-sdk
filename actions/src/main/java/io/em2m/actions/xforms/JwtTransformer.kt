3e0243e550f72949989bb7ed4e1801e0bd87662f
Christopher Lakey <christopherlakey@gmail.com>
2018-06-28 17:38:25 -0400

    Bump dependency version

--- actions/src/main/java/io/em2m/actions/xforms/JwtTransformer.kt
+++ actions/src/main/java/io/em2m/actions/xforms/JwtTransformer.kt
@@ -1,12 +1,12 @@
 package io.em2m.actions.xforms
 
 
-import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.em2m.actions.model.ActionContext
 import io.em2m.actions.model.ActionTransformer
 import io.em2m.actions.model.Problem
 import io.em2m.flows.Priorities
-import org.slf4j.LoggerFactory
 import rx.Observable
 
 class JwtTransformer(val secretKey: String, val requireAuth: Boolean = false, override val priority: Int = Priorities.AUTHENTICATE) : ActionTransformer {
@@ -14,14 +14,9 @@ class JwtTransformer(val secretKey: String, val requireAuth: Boolean = false, ov
     override fun call(obs: Observable<ActionContext>): Observable<ActionContext> {
 
         return obs.doOnNext { context ->
-            val token = context.environment?.get("Token") as? String
+            val token = context.environment["Token"] as? String
             if (token != null && token.isNotEmpty()) {
-                val claims = parseToken(token)
-                if (claims != null) {
-                    context.claims = claims
-                } else {
-                    Problem(status = Problem.Status.NOT_AUTHORIZED, title = "Not Authorized").throwException()
-                }
+                context.claims = parseToken(token)
             } else if (requireAuth) {
                 Problem(status = Problem.Status.NOT_AUTHORIZED, title = "Not Authorized").throwException()
             }
@@ -30,14 +25,12 @@ class JwtTransformer(val secretKey: String, val requireAuth: Boolean = false, ov
 
     private fun parseToken(token: String): Map<String, Any?> {
         return try {
-            val jwtVerifier = JWTVerifier(secretKey)
-            jwtVerifier.verify(token)
+            val algorithm = Algorithm.HMAC256(secretKey)
+            val jwtVerifier = JWT.require(algorithm).build()
+            jwtVerifier.verify(token).claims
         } catch (ex: Exception) {
             Problem(status = Problem.Status.NOT_AUTHORIZED, title = "Not Authorized", detail = ex.message).throwException()
         }
     }
 
-    companion object {
-        val LOG = LoggerFactory.getLogger(JwtTransformer::class.java)
-    }
 }
