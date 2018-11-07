03fff04e885ee4a0947ab7014cb95241a6ef0392
Julien Cruz <julien.cruz@gmail.com>
2018-11-07 15:41:13 -0500

    Updated Lambda action runtime to support API Gateway

--- actions/src/main/java/io/em2m/actions/lambda/LambdaRequest.kt
+++ actions/src/main/java/io/em2m/actions/lambda/LambdaRequest.kt
@@ -1,14 +1,19 @@
 package io.em2m.actions.lambda
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class LambdaRequest(
-        val method: String,
-        val headers: Map<String, String>,
-        val body: String,
-        val requestUri: String,
-        val pathParameters: Map<String, String>,
-        val queryStringParameters: Map<String, String>
+        val method: String? = null,
+        val httpMethod: String? = null,
+        val headers: Map<String, String>? = emptyMap(),
+        val body: String? = null,
+        val requestUri: String?,
+        val path: String? = null,
+        val pathParameters: Map<String, String>? = emptyMap(),
+        val queryStringParameters: Map<String, String>? = emptyMap()
 ) {
     val contentType
-        get() = headers.get("Content-Type")
+        get() = headers?.get("Content-Type")
 }
