28696b1d41026c40cd402524ad499bd1a217e6f2
Christopher Lakey <christopherlakey@gmail.com>
2020-04-17 20:34:11 -0400

    Transform hotfix

--- search/core/src/main/java/io/em2m/search/core/xform/NamedTransformer.kt
+++ search/core/src/main/java/io/em2m/search/core/xform/NamedTransformer.kt
@@ -1,10 +1,8 @@
 package io.em2m.search.core.xform
 
-import io.em2m.search.core.model.Agg
-import io.em2m.search.core.model.SearchRequest
-import io.em2m.search.core.model.Transformer
+import io.em2m.search.core.model.*
 
-class NamedTransformer<T>(namedAggs: Map<String, Agg>) : Transformer<T> {
+class NamedTransformer<T>(val namedAggs: Map<String, Agg>) : Transformer<T> {
 
     // TODO - Timezone needs to propogate down from the request
     private val queryXform = NamedAggQueryTransformer(namedAggs, "America/Los_Angeles")
@@ -14,6 +12,18 @@ class NamedTransformer<T>(namedAggs: Map<String, Agg>) : Transformer<T> {
         val query = request.query?.let { queryXform.transform(it) }
         val aggs = request.aggs.map { aggXform.transform(it) }
         return request.copy(query = query, aggs = aggs)
+    }
+
+    override fun transformResult(request: SearchRequest, result: SearchResult<T>): SearchResult<T> {
+        val aggMap = request.aggs.associateBy { it.key }
+        val aggs = result.aggs.mapValues { (key, aggResult) ->
+            val reqAgg = aggMap[key]
+            if (reqAgg is NamedAgg) {
+                val buckets = aggResult.buckets?.map { it.copy(query = NamedQuery(name = reqAgg.name, value = it.key)) }
+                AggResult(key = key, buckets = buckets, stats = aggResult.stats, value = aggResult.value, op = "filters")
+            } else aggResult
+        }
+        return result.copy(aggs = aggs)
     }
 
 }
