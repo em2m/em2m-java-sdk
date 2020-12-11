81618067866386ab9cbb0a71e219df16f951924b
tkaldahl <tkaldahl90@gmail.com>
2020-12-11 10:03:45 -0500

    do not pass agg format to a dao that may not have format pipe defined

--- search/core/src/main/java/io/em2m/search/core/xform/FieldTransformer.kt
+++ search/core/src/main/java/io/em2m/search/core/xform/FieldTransformer.kt
@@ -212,8 +212,8 @@ class FieldTransformer<T>(val simplex: Simplex, fields: List<FieldModel>) : Tran
             return fields.getOrElse(field, { null })?.delegateFields?.firstOrNull() ?: field
         }
 
-        override fun transformDateHistogramAgg(agg: DateHistogramAgg) = DateHistogramAgg(applyAlias(agg.field), agg.format, agg.interval, agg.offset, agg.timeZone, agg.missing, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
-        override fun transformDateRangeAgg(agg: DateRangeAgg) = DateRangeAgg(applyAlias(agg.field), agg.format, agg.timeZone, agg.ranges, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
+        override fun transformDateHistogramAgg(agg: DateHistogramAgg) = DateHistogramAgg(applyAlias(agg.field), null, agg.interval, agg.offset, agg.timeZone, agg.missing, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
+        override fun transformDateRangeAgg(agg: DateRangeAgg) = DateRangeAgg(applyAlias(agg.field), null, agg.timeZone, agg.ranges, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
         override fun transformFiltersAgg(agg: FiltersAgg) = FiltersAgg(agg.filters.mapValues {
             queryTransformer.transform(it.value)
         }, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
@@ -222,11 +222,11 @@ class FieldTransformer<T>(val simplex: Simplex, fields: List<FieldModel>) : Tran
         override fun transformGeoCentroidAgg(agg: GeoCentroidAgg) = GeoCentroidAgg(applyAlias(agg.field), agg.key, agg.aggs, agg.extensions, agg.minDocCount)
         override fun transformGeoDistanceAgg(agg: GeoDistanceAgg) = GeoDistanceAgg(applyAlias(agg.field), agg.origin, agg.unit, agg.ranges, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
         override fun transformGeoHashAgg(agg: GeoHashAgg) = GeoHashAgg(applyAlias(agg.field), agg.precision, agg.size, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
-        override fun transformHistogramAgg(agg: HistogramAgg) = HistogramAgg(applyAlias(agg.field), agg.format, agg.interval, agg.offset, agg.key, agg.missing, agg.aggs, agg.extensions, agg.minDocCount)
+        override fun transformHistogramAgg(agg: HistogramAgg) = HistogramAgg(applyAlias(agg.field), null, agg.interval, agg.offset, agg.key, agg.missing, agg.aggs, agg.extensions, agg.minDocCount)
         override fun transformMissingAgg(agg: MissingAgg) = MissingAgg(applyAlias(agg.field), agg.key, agg.aggs, agg.extensions, agg.minDocCount)
         override fun transformRangeAgg(agg: RangeAgg) = RangeAgg(applyAlias(agg.field), agg.ranges, agg.key, agg.aggs, agg.extensions, agg.minDocCount)
-        override fun transformStatsAgg(agg: StatsAgg) = StatsAgg(applyAlias(agg.field), agg.key, agg.format, agg.aggs, agg.extensions, agg.minDocCount)
-        override fun transformTermsAgg(agg: TermsAgg) = TermsAgg(applyAlias(agg.field), agg.size, agg.key, agg.sort, agg.format, agg.missing, agg.aggs, agg.extensions, agg.minDocCount)
+        override fun transformStatsAgg(agg: StatsAgg) = StatsAgg(applyAlias(agg.field), agg.key, null, agg.aggs, agg.extensions, agg.minDocCount)
+        override fun transformTermsAgg(agg: TermsAgg) = TermsAgg(applyAlias(agg.field), agg.size, agg.key, agg.sort, null, agg.missing, agg.aggs, agg.extensions, agg.minDocCount)
 
     }
 }
