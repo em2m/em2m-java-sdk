64c6de0d0a2e47e5fb71f632db47cd6c3d2927bd
Christopher Lakey <christopherlakey@gmail.com>
2021-01-13 12:37:23 -0500

    Use scope for policy rewrite statements

--- actions/src/main/java/io/em2m/actions/xforms/RewriteTransformer.kt
+++ actions/src/main/java/io/em2m/actions/xforms/RewriteTransformer.kt
@@ -8,20 +8,37 @@ import io.em2m.actions.model.Priorities
 class RewriteTransformer(override val priority: Int = Priorities.REWRITE) : ActionTransformer {
 
     override fun doOnNext(ctx: ActionContext) {
-        val actionName = ctx.actionName
         val requestContext = ctx.toPolicyContext().map
         val rewrites = ctx.actionCheck?.rewrites ?: emptyList()
-        ctx.rewrites = rewrites.mapNotNull { rewrite ->
+        val result = HashMap<String, Any?>()
+        rewrites.forEach { rewrite ->
             try {
                 if (rewrite.condition.call(requestContext)) {
                     val key = rewrite.key
                     val value = rewrite.value.call(requestContext)
-                     key to value
-                } else null
+                    val replace = rewrite.replace
+                    if (replace) {
+                        result[key] = value
+                    } else {
+                        val current = result[key]
+                        when {
+                            current is List<*> -> {
+                                result[key] = current.plus(value)
+                            }
+                            current != null -> {
+                                result[key] = listOf(current, value)
+                            }
+                            else -> {
+                                result[key] = value
+                            }
+                        }
+                    }
+                }
             } catch (ex: Exception) {
-                null
             }
-        }.toMap()
+        }
+        ctx.rewrites = result
+        ctx.scope.putAll(ctx.rewrites)
     }
 
 }
