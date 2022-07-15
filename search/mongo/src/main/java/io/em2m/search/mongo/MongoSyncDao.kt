4712b149c0531e03d5338aa4b7cb500596693d89
Kyle Rasmussen <krasmus@ip-192-168-1-157.ec2.internal>
2022-07-15 13:07:07 -0400

    Update imports

--- search/mongo/src/main/java/io/em2m/search/mongo/MongoSyncDao.kt
+++ search/mongo/src/main/java/io/em2m/search/mongo/MongoSyncDao.kt
@@ -33,7 +33,6 @@ import org.bson.Document
 import org.bson.conversions.Bson
 import java.util.concurrent.ForkJoinPool
 import java.util.stream.Collectors
-import com.mongodb.client.model.Collation
 
 
 class MongoSyncDao<T>(
