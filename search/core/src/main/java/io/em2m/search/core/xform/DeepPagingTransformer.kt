515f3c995f74ba07de138c1f56ee77c9b2430576
dylanmichaels <dylan.michaels@elasticm2m.com>
2022-10-06 16:30:56 -0400

    DeepPagingTransformer now works with both fielded and item queries

--- search/core/src/main/java/io/em2m/search/core/xform/DeepPagingTransformer.kt
+++ search/core/src/main/java/io/em2m/search/core/xform/DeepPagingTransformer.kt
@@ -2,16 +2,17 @@ package io.em2m.search.core.xform
 
 import io.em2m.search.core.model.*
 import io.em2m.search.core.model.Direction.Descending
+import io.em2m.simplex.evalPath
 import io.em2m.simplex.model.ExprContext
 import io.em2m.utils.coerce
 
 class DeepPagingTransformer<T>(private val idField: String) : Transformer<T> {
 
     override fun transformRequest(request: SearchRequest, context: ExprContext): SearchRequest {
-        return if (request.deepPage && request.fields.isNotEmpty()) {
+        return if (request.deepPage) {
             val last: Map<String, Any?>? = request.params["lastKey"].coerce()
             val sorts = transformSorts(request.sorts)
-            val fields = transformFields(request.fields)
+            val fields = if (request.fields.isNotEmpty()) transformFields(request.fields) else request.fields
             val query = transformQuery(request.query, sorts, last)
             val aggs = request.aggs
             request.copy(sorts = sorts, fields = fields, query = query, aggs = aggs)
@@ -65,7 +66,8 @@ class DeepPagingTransformer<T>(private val idField: String) : Transformer<T> {
         context: ExprContext
     ): SearchResult<T> {
         val lastRow = result.rows?.lastOrNull()
-        return if (request.deepPage && request.fields.isNotEmpty() && lastRow != null) {
+        val lastItem = result.items?.lastOrNull()
+        return if (request.deepPage && lastRow != null) {
             val fieldIndex = result.fields.mapIndexed { index, field ->
                 field.name to index
             }.toMap()
@@ -76,6 +78,12 @@ class DeepPagingTransformer<T>(private val idField: String) : Transformer<T> {
                     key to lastRow[pos]
                 } else null
             }.toMap()
+            result.copy(headers = result.headers.plus("lastKey" to lastKey))
+        } else if (request.deepPage && lastItem != null) {
+            val keys = request.sorts.map { it.field }.toSet().plus(idField)
+            val lastKey = keys.associateWith { key ->
+                lastItem.evalPath(key)
+            }
             result.copy(headers = result.headers.plus("lastKey" to lastKey))
         } else result
     }
