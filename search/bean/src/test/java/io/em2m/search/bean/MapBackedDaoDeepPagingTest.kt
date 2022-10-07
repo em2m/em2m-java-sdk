97c7a7c874ae33f735ad757df13a7e144a2a7d1b
dylanmichaels <dylan.michaels@elasticm2m.com>
2022-10-07 10:18:54 -0400

    add more tests to DeepPagingItemIterable

--- search/bean/src/test/java/io/em2m/search/bean/MapBackedDaoDeepPagingTest.kt
+++ search/bean/src/test/java/io/em2m/search/bean/MapBackedDaoDeepPagingTest.kt
@@ -9,7 +9,8 @@ import kotlin.test.assertEquals
 
 class MapBackedDaoDeepPagingTest {
     private var dao: MapBackedSyncDao<Movie> by Delegates.notNull()
-    private var deepPagingItemIterable: DeepPagingItemIterable<Movie> by Delegates.notNull()
+    private var deepPagingMovieIterable: DeepPagingItemIterable<Movie> by Delegates.notNull()
+    private var emptyDeepPagingItemIterable: DeepPagingItemIterable<Any> by Delegates.notNull()
 
     companion object {
         private var movies = Movie.load()
@@ -18,8 +19,14 @@ class MapBackedDaoDeepPagingTest {
     @Before
     fun setup() {
         dao = MapBackedSyncDao(MovieMapper(), movies)
-        deepPagingItemIterable = DeepPagingItemIterable(
+        deepPagingMovieIterable = DeepPagingItemIterable(
             searchable = dao,
+            sorts = listOf(DocSort("id", Direction.Descending)),
+            idField = "id",
+            chunkSize = 1000
+        )
+        emptyDeepPagingItemIterable = DeepPagingItemIterable(
+            {  SearchResult(totalItems = 0) },
             sorts = listOf(DocSort("id", Direction.Descending)),
             idField = "id",
             chunkSize = 1000
@@ -28,8 +35,41 @@ class MapBackedDaoDeepPagingTest {
 
     @Test
     fun `count function returns correct size`() {
-        val moviesCount = deepPagingItemIterable.count()
+        val moviesCount = deepPagingMovieIterable.count()
         assertEquals(5000, moviesCount)
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun `throws exception for searchable with no results`() {
+        emptyDeepPagingItemIterable.iterator().next()
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun `throws exception when fetching more items fails unexpectedly`() {
+        var numberOfSearches = 0
+        val searchable: Searchable<Movie> = Searchable {
+            val result = when (numberOfSearches) {
+                0 -> dao.search(it)
+                else -> SearchResult(totalItems = 0)
+            }
+            numberOfSearches++
+            result
+        }
+        val brokenDeepPagingItemIterator = DeepPagingItemIterable<Movie>(
+            searchable = searchable,
+            sorts = listOf(DocSort("id", Direction.Descending)),
+            idField = "id",
+            chunkSize = 1000
+        ).iterator()
+        while (brokenDeepPagingItemIterator.hasNext()) {
+            brokenDeepPagingItemIterator.next()
+        }
+    }
+
+    @Test
+    fun `iterates through all results`() {
+        val idsList = deepPagingMovieIterable.map { it.id }
+        assertEquals(5000, idsList.size)
     }
 
     class MovieMapper : IdMapper<Movie> {
