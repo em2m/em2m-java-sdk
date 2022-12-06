925832a81019524ae781fcf33b5728d050a0b2e3
dylanmichaels <dylan.michaels@elasticm2m.com>
2022-12-06 10:41:40 -0500

    make TransformerOptIn non-repeatable

--- actions/src/main/java/io/em2m/actions/model/TransformerOptIn.kt
+++ actions/src/main/java/io/em2m/actions/model/TransformerOptIn.kt
@@ -4,5 +4,4 @@ import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.CLASS)
 @MustBeDocumented
-@Repeatable
 annotation class TransformerOptIn(vararg val transformerOptIns: KClass<out ActionTransformer>)
