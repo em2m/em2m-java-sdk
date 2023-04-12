a780330ed480bb6a3499387301556ba6764fe8c5
Christopher Lakey <christopherlakey@gmail.com>
2023-04-12 16:40:35 -0400

    Fix failing test, remove duplicate test

--- simplex/src/test/java/io/em2m/simplex/ExecTest.kt
+++ simplex/src/test/java/io/em2m/simplex/ExecTest.kt
@@ -5,25 +5,18 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.em2m.simplex.model.*
 import io.em2m.simplex.parser.SimplexModule
 import io.em2m.simplex.std.LogHandler
-import io.em2m.simplex.std.PairHandler
-import org.junit.Assert.assertEquals
 import org.junit.Ignore
 import org.junit.Test
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.test.assertTrue
 
-
 class ExecTest {
 
     private val simplex = Simplex()
 
     init {
-        simplex.execs(BasicExecResolver()
-            .handler("log") { LogHandler() }
-            .handler("http:get") { HttpHandler() }
-            .handler("object:pair") { PairHandler() }
-        )
+        simplex.execs(BasicExecResolver().handler("log") { LogHandler() }.handler("http:get") { HttpHandler() })
         simplex.keys(BasicKeyResolver().key(Key("field", "*"), PathKeyHandler()))
     }
 
@@ -75,32 +68,5 @@ class ExecTest {
         }
 
     }
-
-    @Test
-    @Ignore
-    fun testPair() {
-        val exec: Expr = mapper.readValue(
-            """
-                  {
-                    "@container": [
-                      {
-                         "@exec": "object:pair",
-                         "key": "1_#{key}",
-                         "value": "#{value}"
-                       },
-                      {
-                         "@exec": "object:pair",
-                         "key": "2_#{key}",
-                         "value": "#{value}"
-                      }
-                    ]
-                  }
-                """.replace("#", "$")
-        )
-        val result = exec.call(mapOf("key" to "x", "value" to "y"))
-        assertEquals(mapOf("1_x" to "y", "2_x" to "y"), result)
-    }
-
-
 
 }
