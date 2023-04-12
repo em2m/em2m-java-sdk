f54cdf72e208978a6c023081daa6f46f6abc4f03
Christopher Lakey <christopherlakey@gmail.com>
2023-04-12 15:58:08 -0400

    Add support for creating dynamic objects using object:pair.  Also fix Simplex containers with arrays

--- simplex/src/test/java/io/em2m/simplex/ExecTest.kt
+++ simplex/src/test/java/io/em2m/simplex/ExecTest.kt
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.em2m.simplex.model.*
 import io.em2m.simplex.parser.SimplexModule
 import io.em2m.simplex.std.LogHandler
+import io.em2m.simplex.std.PairHandler
+import org.junit.Assert.assertEquals
 import org.junit.Ignore
 import org.junit.Test
 import java.net.HttpURLConnection
@@ -18,8 +20,9 @@ class ExecTest {
 
     init {
         simplex.execs(BasicExecResolver()
-                .handler("log") { LogHandler() }
-                .handler("http:get") { HttpHandler() }
+            .handler("log") { LogHandler() }
+            .handler("http:get") { HttpHandler() }
+            .handler("object:pair") { PairHandler() }
         )
         simplex.keys(BasicKeyResolver().key(Key("field", "*"), PathKeyHandler()))
     }
@@ -30,12 +33,13 @@ class ExecTest {
     fun testLog() {
 
         val exec: Expr = mapper.readValue(
-                """
+            """
                     {
                      "@exec": "log",
                      "value": "value"
                     }
-                    """)
+                    """
+        )
         val context = HashMap<String, Any?>()
         exec.call(context)
     }
@@ -44,13 +48,14 @@ class ExecTest {
     @Ignore
     fun testHttp() {
         val exec: Expr = mapper.readValue(
-                """
+            """
                  {
                    "@exec": "http:get",
                    "url": "https://jsonplaceholder.typicode.com/posts/1",
                    "@value": "#{result.body}"
                  }
-                """.replace("#", "$"))
+                """.replace("#", "$")
+        )
         val result = exec.call(emptyMap())
         assertTrue(result is String)
         assertTrue(result.startsWith("quia"))
@@ -70,5 +75,32 @@ class ExecTest {
         }
 
     }
+
+    @Test
+    @Ignore
+    fun testPair() {
+        val exec: Expr = mapper.readValue(
+            """
+                  {
+                    "@container": [
+                      {
+                         "@exec": "object:pair",
+                         "key": "1_#{key}",
+                         "value": "#{value}"
+                       },
+                      {
+                         "@exec": "object:pair",
+                         "key": "2_#{key}",
+                         "value": "#{value}"
+                      }
+                    ]
+                  }
+                """.replace("#", "$")
+        )
+        val result = exec.call(mapOf("key" to "x", "value" to "y"))
+        assertEquals(mapOf("1_x" to "y", "2_x" to "y"), result)
+    }
+
+
 
 }
