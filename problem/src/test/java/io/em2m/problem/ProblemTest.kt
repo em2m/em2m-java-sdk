382701c5e887790917a4fb2a5074fb2995e23826
tkaldahl <tkaldahl90@gmail.com>
2024-09-07 14:11:14 -0400

    name tests properly

--- problem/src/test/java/io/em2m/problem/ProblemTest.kt
+++ problem/src/test/java/io/em2m/problem/ProblemTest.kt
@@ -14,16 +14,19 @@ class ProblemTest {
     }
 
     @Test
-    fun filteredCharactersCheck() {
-        val convertedProblem = Problem.convert(IllegalStateException("Could not resolve type id '<script>nefariousFunction()</script>'"))
-        assertEquals("Could not resolve type id 'scriptnefariousFunction()script'", convertedProblem.detail)
+    fun filterCharactersOnProblems() {
+        val problem = Problem(
+            status = Problem.Status.BAD_REQUEST,
+            title = "Error parsing JSON request",
+            detail = "Could not resolve type id '<script>nefariousFunction()</script>'"
+        )
+        assertEquals("Could not resolve type id 'scriptnefariousFunction()script'", problem.detail)
     }
 
     @Test
-    fun ignoredCharactersCheck2() {
-        val problem = Problem(
-            status = Problem.Status.BAD_REQUEST, title = "Error parsing JSON request", detail = "Could not resolve type id '<script>nefariousFunction()</script>'")
-        assertEquals("Could not resolve type id 'scriptnefariousFunction()script'", problem.detail)
+    fun filterCharactersOnConvertedProblems() {
+        val convertedProblem = Problem.convert(IllegalStateException("Could not resolve type id '<script>nefariousFunction()</script>'"))
+        assertEquals("Could not resolve type id 'scriptnefariousFunction()script'", convertedProblem.detail)
     }
 
     @Test
