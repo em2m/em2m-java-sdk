d6286d026efbeefedf26f5a66451905887fd3514
Mike Resnik <mike.resnik@xirgo.com>
2025-12-16 11:17:51 -0500

    adds vararg.

--- transactions/src/main/kotlin/io/em2m/transactions/AbstractOnStateChangeListener.kt
+++ transactions/src/main/kotlin/io/em2m/transactions/AbstractOnStateChangeListener.kt
@@ -2,6 +2,8 @@ package io.em2m.transactions
 
 abstract class AbstractOnStateChangeListener(states: Collection<TransactionState> = TransactionState.entries) : OnStateChangeListener {
 
+    constructor(vararg states: TransactionState): this(states.toList())
+
     protected open val allowedStates = states.toSet()
 
     override fun matches(state: TransactionState): Boolean = state in allowedStates
